59ddaabccc620e05da28ace3cfd7e6c3abb04d8e
Denis Nuțiu <denis.nutiu@gmail.com>
2017-09-11 18:59:41 +0300

    Using go-fmt on leap.go

--- exercism/leap/leap.go
+++ exercism/leap/leap.go
@@ -5,11 +5,11 @@ const testVersion = 3
 // IsLeapYear returns true if the given year is a leap year, false otherwise.
 func IsLeapYear(year int) bool {
 
-	if year % 4 == 0 { // True for every year that is divisible by 4
-		if year % 400 == 0 { // If the year is divisible by 400 it surely is a leap year.
+	if year%4 == 0 { // True for every year that is divisible by 4
+		if year%400 == 0 { // If the year is divisible by 400 it surely is a leap year.
 			return true
 		}
-		if year % 100 == 0 { // Unless it's not divisible by 400 and is divisible by 100
+		if year%100 == 0 { // Unless it's not divisible by 400 and is divisible by 100
 			return false
 		}
 		return true
